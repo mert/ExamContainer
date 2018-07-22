8632cbd8ceb94f1b3c7bbfe3fd5fd43d6b701a0c
mert <mert@spacerabbit.co>
2018-07-22 14:41:25 +0000

    Add Config Generic Host

---
+++ Services/Config/ConfigHost/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace ConfigHost
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddLogging();
+
+                    services.AddSingleton(sp =>
+                    {
+                        return ConnectionMultiplexer.Connect("redis:6379");
+                    });
+                })
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConsole();
+                });
+
+            await builder.RunConsoleAsync();
+        }
+    }
+}
