ab9eb0cb5d0f7e6b1341ccf3e8455edffb4f4333
mert <mertcelep@gmail.com>
2018-07-23 00:48:39 +0300

    Added ServiceA demo web

---
+++ Web/Demo/ServiceA.WebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ServiceA.WebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
