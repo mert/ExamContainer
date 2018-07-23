28a5a498ef465154ed61304144d35307c208152d
mert <mertcelep@gmail.com>
2018-07-23 09:16:49 +0300

    added Infrastructure class lib

---
+++ Infrastructure/Config.Infrastructure/Formatter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Config.Infrastructure
+                
+{
+    public static class Formatter
+    {
+        static BinaryFormatter _binaryFormatter = new BinaryFormatter();
+
+        public static byte[] ToByteArray(object obj)
+        {
+            if (obj == null)
+                return null;
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                _binaryFormatter.Serialize(ms, obj);
+                return ms.ToArray();
+            }
+        }
+
+        public static T FromByteArray<T>(byte[] data)
+        {
+            if (data == null)
+                return default(T);
+
+            using (var ms = new MemoryStream(data))
+            {
+                return (T)_binaryFormatter.Deserialize(ms);
+            }
+        }
+    }
+}
---
+++ Infrastructure/Config.Infrastructure/Models/Event.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace Config.Infrastructure.Models
+{
+    [Serializable]
+    public class Event
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string AppName { get; set; }
+        public EventType Type { get; set; } = EventType.Instance;
+        public double RefreshTimerIntervalInMs { get; set; }
+
+        public Event(string appName, double refleshTimer)
+        {
+            AppName = appName;
+            RefreshTimerIntervalInMs = refleshTimer;
+        }
+
+        public static implicit operator RedisValue(Event @event) {
+            return Formatter.ToByteArray(@event);
+        }
+    }
+
+    public enum EventType {
+        Instance,
+        Disposing
+    }
+}
